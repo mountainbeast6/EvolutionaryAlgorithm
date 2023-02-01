0f661e1c8bdb3313e077b68e22770f506ee61e91
mountainbeast6 <mountainbeastcarbon6@gmail.com>
2023-02-01 12:33:32 -0800

    Physics somewhat working

--- main.py
+++ main.py
@@ -2,41 +2,54 @@ import random
 import pygame
 from player import *
 
-#start the pygame engine
+# start the pygame engine
 pygame.init()
 
-#start the pygame font engine
+# start the pygame font engine
 pygame.font.init()
-myfont = pygame.font.SysFont('Comic Sans MS', 23) #load a font for use
+myfont = pygame.font.SysFont('Comic Sans MS', 23)  # load a font for use
 
-#start the sound engine
-pygame.mixer.init();
+# start the sound engine
+pygame.mixer.init()
 
-#game variables
+# game variables
 simOver = False
-p1=Player()
-#game independent variables (needed for every pygame)
-FPS = 60 #60 Frames Per Second for the game update cycle
-fpsClock = pygame.time.Clock() #used to lock the game to 60 FPS
-screen = pygame.display.set_mode((1280,720)); #initialize the game window
+
+map1 = Map(-12)
+p1 = Player(map1)
+# game independent variables (needed for every pygame)
+FPS = 60  # 60 Frames Per Second for the game update cycle
+fpsClock = pygame.time.Clock()  # used to lock the game to 60 FPS
+screen = pygame.display.set_mode((1280, 720))  # initialize the game window
+
 
 def clear_screen():
-    pygame.draw.rect(screen, (0,0,0), (0, 0, 1280, 720))
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, 1280, 720))
 
 
-#main while loop
+def create_map_1():
+    map1.add(Platform(100, 500, 400, 30, (145, 105, 200)))
+    map1.add(Platform(600, 600, 100, 30, (145, 105, 200)))
+    map1.set_gravity(-0.3)
+
+
+create_map_1()
+p1.set_map(map1)
+
+# main while loop
 while not simOver:
-    #loop through and empty the event queue, key presses
-    #buttons, clicks, etc.
+    # loop through and empty the event queue, key presses
+    # buttons, clicks, etc.
     for event in pygame.event.get():
 
-        #if the event is a click on the "X" close button
+        # if the event is a click on the "X" close button
         if event.type == pygame.QUIT:
             simOver = True
+    clear_screen()
     p1.draw(screen)
-
-
-    #put all the graphics on the screen
-    #should be the LAST LINE of game code
+    map1.draw(screen)
+    p1.act()
+    # put all the graphics on the screen
+    # should be the LAST LINE of game code
     pygame.display.flip()
-    fpsClock.tick(FPS) #slow the loop down to 60 loops per second
+    fpsClock.tick(FPS)  # slow the loop down to 60 loops per second
--- player.py
+++ player.py
@@ -1,12 +1,98 @@
 import pygame
+
+
 class Player:
 
-        def __init__(self):
-            self.x=400
-            self.y=400
-            self.width = 30
-            self.height=30
-            self.color=(255,0,0)
+    def __init__(self, map):
+        self.x = 400
+        self.y = 400
+        self.width = 30
+        self.height = 30
+        self.color = (255, 0, 0)
+        self.map = map
+        self.isJumping = False
+        self.velocity=0;
+        self.x_velocity=0
 
-        def draw(self, screen):
-            pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+
+    def set_map(self, map):
+        self.map = map
+
+    def handle_key_presses(self):
+        if pygame.key.get_pressed()[pygame.K_SPACE] and self.isJumping <= 4:
+            self.jump()
+            print("works")
+            self.isJumping+=1
+        if pygame.key.get_pressed()[pygame.K_a] and self.x_velocity.__abs__() <= 5:
+            self.x_velocity-=2;
+        if pygame.key.get_pressed()[pygame.K_d] and self.x_velocity.__abs__() <= 5:
+            self.x_velocity+=2;
+
+    def act(self):
+        oldx=self.x
+        oldy=self.y
+        self.handle_key_presses()
+        self.velocity-=self.map.get_gravity()
+        self.y += self.velocity
+        self.x +=self.x_velocity
+        if self.x_velocity<0:
+            self.x_velocity+=0.5
+        if self.x_velocity>0:
+            self.x_velocity-=0.5
+        if self.is_map_collision():
+            self.y = oldy
+            if self.velocity<0.1:
+                self.velocity=0
+            else:
+                self.velocity*=-1/2
+            self.isJumping=0
+
+    def jump(self):
+        self.velocity-=1.5
+
+    def is_map_collision(self):
+        plat =self.map.get_hit_boxes()
+        for p in plat:
+            if p.get_rect().colliderect(pygame.Rect(self.x,self.y,self.width,self.height)):
+                return True
+        return False
+
+
+
+class Platform:
+
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def get_rect(self):
+        return pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.get_rect())
+
+
+class Map:
+    def __init__(self, gravity=-0.5):
+        self.platforms = []
+        self.gravity = gravity
+
+    def get_hit_boxes(self):
+        return self.platforms
+
+    def add(self, platform):
+        self.platforms.append(platform)
+
+        # Precondition: homogeneous list of platform objects
+    def get_gravity(self):
+        return self.gravity
+    def set_gravity(self, gravity):
+        self.gravity=gravity;
+    def draw(self, screen):
+        for p in self.platforms:
+            p.draw(screen)
